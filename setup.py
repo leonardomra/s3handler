fc0f6b25b6e2b676d67e4d90bb14154a37861bc9
Leonardo de Araujo <dearaujo@uni-bremen.de>
2020-11-19 16:32:31 +0100

    updated github repo

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="AWS S3 Data Management.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/leonardomra/s3handler",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
